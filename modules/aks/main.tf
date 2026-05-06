e22f19dc4f79256ed059caa759e5bf7b17d372bf
sallaabhilash <sallaabhilash@outlook.com>
2026-05-06 07:29:17 +0530

    Update VM size and location for AKS cluster

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -2,18 +2,18 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name = var.name
   location = var.location
   resource_group_name = var.rg_name
-  dns_prefix = "aksdemo"
+  dns_prefix = "aks-sir1-dev"
 
   default_node_pool {
     name = "default"
     node_count = var.node_count
-    vm_size = "Standard_B2ms"
+    vm_size = "Standard_D2s_v4"
     vnet_subnet_id = var.subnetid
   }
 network_profile {
   network_plugin = "azure"
-  service_cidr = var.subnet_cidr
-  dns_service_ip = var.dns_service_ip
+  service_cidr = "172.16.0.0/16"
+  dns_service_ip = "172.16.0.10"
 }
   identity {
     type = "SystemAssigned"
